bbcfd7b34f17ada616b269ab2705960eb33a0623
Vedant Vohra <vedant.vohra1@gmail.com>
2018-01-22 15:31:41 +0530

    implemented spelling correction

--- sentiment_classifier/senti_lite.py
+++ sentiment_classifier/senti_lite.py
@@ -2,7 +2,7 @@ from textblob import TextBlob
 
 
 def classify(text):
-    blob = TextBlob(text)
+    blob = TextBlob(text).correct()
     res = {'polarity': round(blob.sentiment.polarity, 1)}
 
     return res
