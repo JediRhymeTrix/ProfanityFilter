bb07c44e4b97d3db41e2b5253f253bb045f419e6
JediRhymeTrix <ved914980@gmail.com>
2018-02-07 16:14:01 +0530

    added more data

--- FeedList.py
+++ FeedList.py
@@ -1,14 +1,23 @@
 import sys
 import sqlite3
 
-db_name = 'DirtyWords.db'
+db_name = './data/DirtyWords.db'
 conn = sqlite3.connect(db_name)
-
+conn.text_factory = str
 
 def feed(src, lang):
     for word in open(src, 'r'):
         t = (word.strip(), lang, )
-        conn.execute('INSERT INTO dirty_words (word, language) values(?, ?)', t)
+        try:
+            conn.execute('INSERT INTO dirty_words (word, language) values(?, ?)', t)
+        except Exception as e:
+            try:
+                print 'skipped duplicate - ', t[0]
+            except Exception as e:
+                continue
+    print 'Done!'
+    conn.commit()
+    conn.close()
 
 
-feed('test.txt', 'en')
+feed('./data/feed_list.txt', 'en')
